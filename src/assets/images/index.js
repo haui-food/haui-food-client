77367bedfbe186dd975b5867a8de21abca69cb41
Công <yeusangtao96@gmail.com>
2024-03-13 20:46:50 +0700

    #1 - update header

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,7 @@
 const images = {
   logo: require('~/assets/images/logo.png'),
   logo1: require('~/assets/images/logo1.png'),
+  cart: require('~/assets/images/cart.svg').default,
 };
 
 export default images;
