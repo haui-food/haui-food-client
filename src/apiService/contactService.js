8029e7a795d20cee4cdafc163cbec465b1f967cf
Công <yeusangtao96@gmail.com>
2024-05-20 20:02:53 +0700

    Update API

--- src/apiService/contactService.js
+++ src/apiService/contactService.js
@@ -1,14 +1,15 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import hostname from '~/utils/http';
-import axios from 'axios';
 import Cookies from 'js-cookie';
+import { callApi } from './apiUtils';
 
-export const contactUs = createAsyncThunk('contacts', async (communications) => {
+export const contactUs = createAsyncThunk('contacts', async (communications, { rejectWithValue }) => {
   try {
-    const req = await axios.post(`${hostname}/v1/contacts?lang=${Cookies.get('lang')}`, communications);
-    const res = req.data.data;
+    const customHeaders = {
+      'accept-language': `${Cookies.get('lang')}`,
+    };
+    const res = await callApi('post', `/v1/contacts`, null, communications, customHeaders);
     return res;
   } catch (err) {
-    throw err.response !== null ? new Error(err.response.data.message) : new Error('Đã xảy ra lỗi không mong đợi');
+    return rejectWithValue({ ...err });
   }
 });
